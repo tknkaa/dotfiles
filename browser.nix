7679a201ccbc3208546d025890c6c727c6706c9a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-14 12:49:59 +0900

    fix chrome scaling

--- browser.nix
+++ browser.nix
@@ -1,7 +1,12 @@
 {
   programs = {
     firefox.enable = true;
-    google-chrome.enable = true;
+    google-chrome = {
+      enable = true;
+      commandLineArgs = [
+        "--force-device-scale-factor=1.2"
+      ];
+    };
     vivaldi = {
       enable = true;
       commandLineArgs = [ "--enable-features=WebUIDarkMode" "--force-dark-mode" ];
