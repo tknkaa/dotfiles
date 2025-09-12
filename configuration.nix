a1549ce2ced85fd2499043da15af886cab81145c
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-12 14:23:13 +0800

    use us layout

--- configuration.nix
+++ configuration.nix
@@ -27,21 +27,9 @@
   time.timeZone = "Asia/Singapore";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  #  i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "en_US.UTF-8";
-  #   LC_IDENTIFICATION = "en_US.UTF-8";
-  #   LC_MEASUREMENT = "en_US.UTF-8";
-  #   LC_MONETARY = "en_US.UTF-8";
-  #   LC_NAME = "en_US.UTF-8";
-  #   LC_NUMERIC = "en_US.UTF-8";
-  #   LC_PAPER = "en_US.UTF-8";
-  #   LC_TELEPHONE = "en_US.UTF-8";
-  #   LC_TIME = "en_US.UTF-8";
-  #  };
-
-  console.keyMap = "jp106";
+  console.keyMap = "us";
 
   # Configure keymap in X11
   services.xserver = {
@@ -49,7 +37,7 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     xkbVariant = "";
-    layout = "jp";
+    layout = "us";
   };
 
   # Enable CUPS to print documents.
