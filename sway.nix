90b814d68e5f17634b41baf60548cd66c3a11699
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-13 16:04:04 +0800

    Revert "toggle float"
    This reverts commit 97b9680d53ab517d9a982949309e5e635675c64f.

--- sway.nix
+++ sway.nix
@@ -12,7 +12,6 @@
         "${modifier}+Return" = "exec ${terminal}";
         "${modifier}+Shift+q" = "kill";
         "${modifier}+d" = "exec wofi --show drun";
-        "${modifier}+Shift+Space" = "floating toggle";
       };
     };
   };
