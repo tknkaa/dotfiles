1391da9f4f245107daa65da482c1bb9ce2cf5165
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-13 16:04:16 +0800

    Revert "use us layout"
    This reverts commit a1549ce2ced85fd2499043da15af886cab81145c.

--- configuration.nix
+++ configuration.nix
@@ -27,9 +27,21 @@
   time.timeZone = "Asia/Singapore";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "ja_JP.UTF-8";
 
-  console.keyMap = "us";
+  #  i18n.extraLocaleSettings = {
+  #   LC_ADDRESS = "en_US.UTF-8";
+  #   LC_IDENTIFICATION = "en_US.UTF-8";
+  #   LC_MEASUREMENT = "en_US.UTF-8";
+  #   LC_MONETARY = "en_US.UTF-8";
+  #   LC_NAME = "en_US.UTF-8";
+  #   LC_NUMERIC = "en_US.UTF-8";
+  #   LC_PAPER = "en_US.UTF-8";
+  #   LC_TELEPHONE = "en_US.UTF-8";
+  #   LC_TIME = "en_US.UTF-8";
+  #  };
+
+  console.keyMap = "jp106";
 
   # Configure keymap in X11
   services.xserver = {
@@ -37,7 +49,7 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     xkbVariant = "";
-    layout = "us";
+    layout = "jp";
   };
 
   # Enable CUPS to print documents.
