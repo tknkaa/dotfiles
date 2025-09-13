97b9680d53ab517d9a982949309e5e635675c64f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-13 11:05:43 +0800

    toggle float

--- sway.nix
+++ sway.nix
@@ -12,6 +12,7 @@
         "${modifier}+Return" = "exec ${terminal}";
         "${modifier}+Shift+q" = "kill";
         "${modifier}+d" = "exec wofi --show drun";
+        "${modifier}+Shift+Space" = "floating toggle";
       };
     };
   };
