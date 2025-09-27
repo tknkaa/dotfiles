1ab9dcae22ee9c1abb153f6bbf320a806d5fdc38
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-27 11:24:51 +0800

    setup c, python and lua lsp

--- init.lua
+++ init.lua
@@ -14,12 +14,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, 
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", branch = "master", lazy = false },
+  { "nvim-telescope/telescope.nvim", tag = '0.1.8' },
   { "neovim/nvim-lspconfig" },
   { "nvim-lualine/lualine.nvim" },
 })
 
+vim.o.clipboard = "unnamedplus"
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.expandtab = true
@@ -33,9 +39,18 @@ local opts = { noremap = true, silent = true }
 map("i", "jk", "<ESC>", opts)
 map("n", "<leader>e", ":Explore<CR>", opts)
 map("n", "<leader>f", ":Telescope find_files<CR>", opts)
+map("n", "K", vim.lsp.buf.hover, opts)
 
 require("lualine").setup()
 require("nvim-treesitter.configs").setup({
+  ensure_installed = { "c", "lua", "python" },
+  sync_install = false,
+  auto_install = true,
   highlight = { enable = true },
   indent = { enable = true },
 })
+
+-- alr installed pyroght, clangd and lua-language-server from home-manager
+vim.lsp.enable('pyright')
+vim.lsp.enable('clangd')
+vim.lsp.enable('lua_ls')
--- neovim.nix
+++ neovim.nix
@@ -1,8 +1,14 @@
+{ pkgs,... }:
 {
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
+    extraPackages = with pkgs; [
+      clang-tools
+      lua-language-server
+      pyright
+    ];
     extraLuaConfig = builtins.readFile ./init.lua;
   };
 }
