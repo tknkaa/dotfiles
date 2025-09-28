57c8fa8bc16a51369ec5eb4d7e29a9c0f335212d
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-28 10:12:45 +0800

    setup zed theme

--- home.nix
+++ home.nix
@@ -35,7 +35,7 @@
       openssl
       nixfmt-tree
       gemini-cli
-      unzip
+      nixfmt
     ];
     pointerCursor = {
       name = "Adwaita";
--- zed-editor.nix
+++ zed-editor.nix
@@ -1,11 +1,30 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.zed-editor = {
     enable = true;
+    extensions = [
+      "nix"
+      "toml"
+      "lua"
+      "dracula"
+    ];
+    extraPackages = [ pkgs.nixd ];
     userSettings = {
       vim_mode = true;
       theme = "Dracula";
       ui_font_size = 20;
       buffer_font_size = 20;
+      relative_line_numbers = true;
+      lsp = {
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+      };
     };
 
     userKeymaps = [
