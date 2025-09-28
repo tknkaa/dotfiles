19ac068c1b6b197c8ad63638fde591817718688b
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-28 18:24:09 +0800

    configure rust-analyzer

--- zed-editor.nix
+++ zed-editor.nix
@@ -11,17 +11,53 @@
       "lua"
       "dracula"
     ];
-    extraPackages = [ pkgs.nixd ];
+    extraPackages = with pkgs; [
+      nixd
+      nil
+    ];
     userSettings = {
       vim_mode = true;
       theme = "Dracula";
-      ui_font_size = 20;
-      buffer_font_size = 20;
+      ui_font_size = 24;
+      buffer_font_size = 24;
       relative_line_numbers = true;
       lsp = {
         nix = {
           binary = {
             path_lookup = true;
+          };
+        };
+        rust-analyzer = {
+          initialization_options = {
+            checkOnSave = {
+              enable = false;
+            };
+            settings = {
+              procMacro = {
+                enable = false;
+              };
+              cargo = {
+                runBuildScripts = false;
+              };
+              inlayHints = {
+                enable = false;
+              };
+              lens = {
+                enable = false;
+              };
+              hover = {
+                enable = true;
+              };
+              completion = {
+                enable = true;
+              };
+              diagnostics = {
+                enable = false;
+              };
+              callInfo = {
+                enable = false;
+              };
+            };
           };
         };
       };
