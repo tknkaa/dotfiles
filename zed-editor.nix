8562cd4585348c92b0a81be8457e93aa9e45d93b
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-29 17:39:46 +0800

    setup tokyonight theme

--- zed-editor.nix
+++ zed-editor.nix
@@ -1,75 +1,21 @@
 {
-  pkgs,
+  config,
   ...
 }:
 {
   programs.zed-editor = {
     enable = true;
-    extensions = [
-      "nix"
-      "toml"
-      "lua"
-      "dracula"
-    ];
-    extraPackages = with pkgs; [
-      nixd
-      nil
-    ];
-    userSettings = {
-      vim_mode = true;
-      theme = "Dracula";
-      ui_font_size = 24;
-      buffer_font_size = 24;
-      relative_line_numbers = true;
-      lsp = {
-        nix = {
-          binary = {
-            path_lookup = true;
-          };
-        };
-        rust-analyzer = {
-          initialization_options = {
-            checkOnSave = {
-              enable = false;
-            };
-            settings = {
-              procMacro = {
-                enable = false;
-              };
-              cargo = {
-                runBuildScripts = false;
-              };
-              inlayHints = {
-                enable = false;
-              };
-              lens = {
-                enable = false;
-              };
-              hover = {
-                enable = true;
-              };
-              completion = {
-                enable = true;
-              };
-              diagnostics = {
-                enable = false;
-              };
-              callInfo = {
-                enable = false;
-              };
-            };
-          };
-        };
-      };
-    };
+  };
 
-    userKeymaps = [
-      {
-        context = "Editor && vim_mode == insert && !menu";
-        bindings = {
-          "j k" = "vim::NormalBefore";
-        };
-      }
-    ];
+  # Create symlinks for Zed configuration files
+  xdg.configFile = {
+    "zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/.config/zed/settings.json";
+
+     "zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/.config/zed/keymap.json";
+
+     "zed/themes".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/.config/zed/themes";
   };
 }
