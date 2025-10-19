9be7b9a0f91b6e576c1dc12d37bc43b5e412cf9a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-10-19 23:16:31 +0800

    correct path

--- vscode.nix
+++ vscode.nix
@@ -4,7 +4,7 @@
 }:
 {
   xdg.configFile = {
-    "Code/settings.json".source = config.lib.file.mkOutOfStoreSymlink
+    "Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink
       "${config.home.homeDirectory}/.dotfiles/.config/vscode/settings.json";
   };
 }
