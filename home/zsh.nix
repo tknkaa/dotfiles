5130f99c6498bb73aa1a9048603215be1f793f48
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-20 12:10:00 +0800

    chore(zsh): disable autocd

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,7 +1,7 @@
 {
   programs.zsh = {
     enable = true;
-    autocd = true; # cdなしでファイルパスだけで移動
+    autocd = false; # cdなしでファイルパスだけで移動
     enableCompletion = true; # 自動補完
     autosuggestion.enable = true; # 入力サジェスト
     syntaxHighlighting.enable = true; # シンタックスハイライト
