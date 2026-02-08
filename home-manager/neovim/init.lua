f5f7a6660ddfcfe5fd7f990701672548cce1b01e
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-08 11:42:13 +0800

    refactor: reorganize Neovim plugin configuration and apply consistent formatting

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -4,41 +4,173 @@ vim.g.loaded_netrw = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- LSPサーバーリスト
+local lsp_servers = {
+	"pyright",
+	"clangd",
+	"lua_ls",
+	"ts_ls",
+	"svelte",
+	"gopls",
+	"r_language_server",
+	"nixd",
+	"gleam",
+	"rust_analyzer",
+	"tinymist",
+	"zls",
+}
+
 require("lazy").setup({
-    {
-        "Mofiqul/vscode.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require('vscode').setup({
-                transparent = false,
-                italic_comments = true,
-                disable_nvimtree_bg = true,
-            })
-            vim.cmd([[colorscheme vscode]])
-        end
-    }, {
-        "yorickpeterse/vim-paper",
-        lazy = true
-    }, {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        branch = "master",
-        lazy = false
-    }, {"nvim-telescope/telescope.nvim", tag = "0.1.8"},
-    {"neovim/nvim-lspconfig"}, {"nvim-lualine/lualine.nvim"},
-    {"hrsh7th/nvim-cmp"}, {"hrsh7th/cmp-nvim-lsp"}, {"hrsh7th/cmp-buffer"},
-    {"hrsh7th/cmp-path"}, {"nvim-tree/nvim-tree.lua"},
-    {"windwp/nvim-autopairs", event = "InsertEnter", config = true}
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vscode").setup({
+				transparent = false,
+				italic_comments = true,
+				disable_nvimtree_bg = true,
+			})
+			vim.cmd([[colorscheme vscode]])
+		end,
+	},
+	{
+		"yorickpeterse/vim-paper",
+		lazy = true,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		branch = "master",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"python",
+					"typescript",
+					"tsx",
+					"javascript",
+					"svelte",
+					"gleam",
+					"go",
+					"r",
+					"nix",
+					"rust",
+					"typst",
+					"zig",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- すべてのLSPに共通の設定
+			for _, server in ipairs(lsp_servers) do
+				vim.lsp.config[server] = {
+					capabilities = capabilities,
+				}
+			end
+
+			-- TypeScript固有の設定
+			vim.lsp.config.ts_ls = {
+				filetypes = {
+					"typescript",
+					"typescriptreact",
+					"javascript",
+					"javascriptreact",
+				},
+				capabilities = capabilities,
+			}
+
+      vim.lsp.config.zls = {
+        capabilities = capabilities,
+        settings = {
+          zls = {
+            enable_build_on_save = true,
+            build_on_save_step = "check",
+          },
+        },
+      }
+
+			-- LSPを有効化
+			for _, server in ipairs(lsp_servers) do
+				vim.lsp.enable(server)
+			end
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
 })
 
+-- 基本設定
 vim.o.clipboard = "unnamedplus"
 vim.o.number = true
 vim.o.relativenumber = true
@@ -49,75 +181,15 @@ vim.o.termguicolors = true
 vim.o.cursorline = true
 -- To switch to light theme, run: :colorscheme paper
 
-require("nvim-tree").setup()
+-- キーマップ設定
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-local map = vim.keymap.set -- shortcut for keymap.set
-local opts = {noremap = true, silent = true}
-map("i", "jk", "<ESC>", opts) -- shortcut: 'jk' in insert mode to escape
-map("n", "<leader>f", ":Telescope find_files<CR>", opts) -- shortcut: <Space>f to Telescope file finder
-map("n", "K", vim.lsp.buf.hover, opts) -- shortcut: 'K' for LSP hover
-map("n", "<C-k>", vim.diagnostic.open_float, opts) -- shortcut: Ctrl+K to show diagnostics/errors
-map("n", "[d", vim.diagnostic.goto_prev, opts) -- shortcut: '[d' to go to previous diagnostic
-map("n", "]d", vim.diagnostic.goto_next, opts) -- shortcut: ']d' to go to next diagnostic
-
-map("n", "<leader>e", ":NvimTreeToggle<CR>", opts) -- shortcut: <Space>e to toggle nvim-tree
-map("n", "<leader>b", ":NvimTreeFocus<CR>", opts) -- shortcut: <Space>b to focus nvim-tree
-
-require("lualine").setup()
-require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-        "c", "lua", "python", "typescript", "tsx", "javascript", "svelte", "gleam",
-        "go", "r", "nix", "rust", "typst", "zig"
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {enable = true},
-    indent = {enable = true}
-})
-
-local cmp = require("cmp")
-cmp.setup({
-    mapping = cmp.mapping.preset.insert({ -- shortcut for completion menu
-        ["<C-Space>"] = cmp.mapping.complete(), -- shortcut: Ctrl+Space to trigger completion
-        ["<C-e>"] = cmp.mapping.abort(), -- shortcut: Ctrl+e to abort completion
-        ["<CR>"] = cmp.mapping.confirm({select = true}), -- shortcut: Enter to confirm completion
-        ["<Tab>"] = cmp.mapping.select_next_item(), -- shortcut: Tab to next completion
-        ["<S-Tab>"] = cmp.mapping.select_prev_item() -- shortcut: Shift+Tab to previous completion
-    }),
-    sources = cmp.config.sources({
-        {name = "nvim_lsp"}, {name = "buffer"}, {name = "path"}
-    })
-})
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-vim.lsp.config.pyright = {capabilities = capabilities}
-vim.lsp.config.clangd = {capabilities = capabilities}
-vim.lsp.config.gopls = {capabilities = capabilities}
-vim.lsp.config.lua_ls = {capabilities = capabilities}
-vim.lsp.config.ts_ls = {
-    filetypes = {
-        "typescript", "typescriptreact", "javascript", "javascriptreact"
-    },
-    capabilities = capabilities
-}
-vim.lsp.config.svelte = {capabilities = capabilities}
-vim.lsp.config.r_language_server = {capabilities = capabilities}
-vim.lsp.config.nixd = {capabilities = capabilities}
-vim.lsp.config.gleam = {capabilities = capabilities}
-vim.lsp.config.rust_analyzer = {capabilities = capabilities}
-vim.lsp.config.tinymist = {capabilities = capabilities}
-vim.lsp.config.zls = {capabilities = capabilities}
-
-vim.lsp.enable("pyright")
-vim.lsp.enable("clangd")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("ts_ls")
-vim.lsp.enable("svelte")
-vim.lsp.enable("gopls")
-vim.lsp.enable("r_language_server")
-vim.lsp.enable("nixd")
-vim.lsp.enable("gleam")
-vim.lsp.enable("rust_analyzer")
-vim.lsp.enable("tinymist")
-vim.lsp.enable("zls")
+map("i", "jk", "<ESC>", opts)
+map("n", "<leader>f", ":Telescope find_files<CR>", opts)
+map("n", "K", vim.lsp.buf.hover, opts)
+map("n", "<C-k>", vim.diagnostic.open_float, opts)
+map("n", "[d", vim.diagnostic.goto_prev, opts)
+map("n", "]d", vim.diagnostic.goto_next, opts)
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "<leader>b", ":NvimTreeFocus<CR>", opts)
--- home-manager/wezterm/wezterm.lua
+++ home-manager/wezterm/wezterm.lua
@@ -2,68 +2,70 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-    -- Theme
-    color_scheme = "Dracula+",
+	-- Theme
+	color_scheme = "Dracula+",
 
-    -- Font
-    font = wezterm.font_with_fallback({
-        {family = "JetBrainsMono Nerd Font", weight = "Regular"}, {
-            family = "JetBrainsMono Nerd Font",
-            weight = "Regular",
-            assume_emoji_presentation = true
-        }, {family = "Noto Sans CJK JP"}
-    }),
-    font_size = 14.0,
+	-- Font
+	font = wezterm.font_with_fallback({
+		{ family = "JetBrainsMono Nerd Font", weight = "Regular" },
+		{
+			family = "JetBrainsMono Nerd Font",
+			weight = "Regular",
+			assume_emoji_presentation = true,
+		},
+		{ family = "Noto Sans CJK JP" },
+	}),
+	font_size = 14.0,
 
-    -- Padding
-    window_padding = {left = 10, right = 10, top = 10, bottom = 10},
+	-- Padding
+	window_padding = { left = 10, right = 10, top = 10, bottom = 10 },
 
-    -- Tab
-    use_fancy_tab_bar = false,
-    tab_bar_at_bottom = true,
-    hide_tab_bar_if_only_one_tab = true,
+	-- Tab
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
 
-    -- Misc
-    use_ime = true, -- Enable IME
-    check_for_updates = false, -- Disable update check
-    audible_bell = "Disabled", -- Disable bell
+	-- Misc
+	use_ime = true, -- Enable IME
+	check_for_updates = false, -- Disable update check
+	audible_bell = "Disabled", -- Disable bell
 
-    keys = {
-        -- 1. 全画面表示 (既存の設定)
-        {key = "n", mods = "SHIFT|CTRL", action = act.ToggleFullScreen},
+	keys = {
+		-- 1. 全画面表示 (既存の設定)
+		{ key = "n", mods = "SHIFT|CTRL", action = act.ToggleFullScreen },
 
-        -- 2. タブの操作
-        {key = "t", mods = "SHIFT|CTRL", action = act.SpawnTab("DefaultDomain")}, -- タブ追加
-        {
-            key = "w",
-            mods = "SHIFT|CTRL",
-            action = act.CloseCurrentTab({confirm = true})
-        }, -- タブを閉じる
-        {key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1)}, -- 次のタブへ (Ctrl + Tab)
-        {key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1)}, -- 前のタブへ (Ctrl + Shift + Tab)
+		-- 2. タブの操作
+		{ key = "t", mods = "SHIFT|CTRL", action = act.SpawnTab("DefaultDomain") }, -- タブ追加
+		{
+			key = "w",
+			mods = "SHIFT|CTRL",
+			action = act.CloseCurrentTab({ confirm = true }),
+		}, -- タブを閉じる
+		{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) }, -- 次のタブへ (Ctrl + Tab)
+		{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) }, -- 前のタブへ (Ctrl + Shift + Tab)
 
-        -- 3. ペインの分割 (縦に分割 / 横に分割)
-        {
-            key = "v",
-            mods = "ALT",
-            action = act.SplitHorizontal({domain = "CurrentPaneDomain"})
-        }, -- 垂直に割る(右に開く)
-        {
-            key = "s",
-            mods = "ALT",
-            action = act.SplitVertical({domain = "CurrentPaneDomain"})
-        }, -- 水平に割る(下に開く)
-        -- 4. ペインの移動 (矢印キーで移動)
-        {key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left")},
-        {key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right")},
-        {key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up")},
-        {key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down")},
+		-- 3. ペインの分割 (縦に分割 / 横に分割)
+		{
+			key = "v",
+			mods = "ALT",
+			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		}, -- 垂直に割る(右に開く)
+		{
+			key = "s",
+			mods = "ALT",
+			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+		}, -- 水平に割る(下に開く)
+		-- 4. ペインの移動 (矢印キーで移動)
+		{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
 
-        -- 5. ペインを閉じる
-        {
-            key = "x",
-            mods = "ALT",
-            action = act.CloseCurrentPane({confirm = true})
-        }
-    }
+		-- 5. ペインを閉じる
+		{
+			key = "x",
+			mods = "ALT",
+			action = act.CloseCurrentPane({ confirm = true }),
+		},
+	},
 }
