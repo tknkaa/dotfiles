d879c6879d54c5fcb2326c87b802805864f5ae82
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-21 11:48:59 +0800

    chore: Add SSL_CERT_FILE environment variable export

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -1,7 +1,7 @@
 {
   programs.zsh = {
     enable = true;
-    autocd = false; # cdなしでファイルパスだけで移動
+    autocd = false;
     enableCompletion = true; # 自動補完
     autosuggestion.enable = true; # 入力サジェスト
     syntaxHighlighting.enable = true; # シンタックスハイライト
@@ -19,6 +19,7 @@
       export KAGGLE_API_TOKEN="$(cat /run/secrets/kaggle_api_token)"
       export PATH="$HOME/.cargo/bin:$PATH"
       export OPENSSL_DIR="$(dirname $(dirname $(which openssl)))"
+      export SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt"
       eval "$(zoxide init zsh --cmd cd)"
       cdi() {
         local dir
