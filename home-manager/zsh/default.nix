681f1028514f7fe08f5814367d472e0a249031c1
tknkaa <tknkaa@users.noreply.github.com>
2026-03-09 08:14:44 +0800

    chore: Disable zeno-completion tab binding

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -53,7 +53,7 @@
       if [[ -n $ZENO_LOADED ]]; then
         bindkey ' '  zeno-auto-snippet
         bindkey '^m' zeno-auto-snippet-and-accept-line
-        bindkey '^i'  zeno-completion
+        # bindkey '^i'  zeno-completion
         bindkey '^x ' zeno-insert-snippet
       fi
     '';
