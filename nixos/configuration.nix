5a393cc18524a16a421c0b6a0d7bd912658e7887
tknkaa <tknkaa@users.noreply.github.com>
2026-03-13 21:39:42 +0800

    feat: Replace Docker with Podman

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -99,27 +99,16 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  virtualisation.docker = {
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-      daemon.settings = {
-        default-address-pools = [
-          { base = "192.168.200.0/24"; size = 24; }
-        ];
-      };
-    };
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     libreoffice-qt
-    docker-compose
+    podman-compose
   ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
